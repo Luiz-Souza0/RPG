c89feae38599f33c9be08bdc8d6326ea6c353c48
Luiz-Souza0 <114114974+Luiz-Souza0@users.noreply.github.com>
2025-10-10 17:52:16 -0300

    Update dadosPersonagem.py

--- dadosPersonagem.py
+++ dadosPersonagem.py
@@ -1,7 +1,7 @@
 # dados personagem
 import random 
-
-def gerar_valores_aleatorios(nome):
+import streamlit as st
+def gerar_valores_aleatorios(nome, Classe, RacaEscolhida):
     Nome = nome
     Atk   = random.randint(1, 18)
     Des   = random.randint(1, 18)
@@ -10,32 +10,60 @@ def gerar_valores_aleatorios(nome):
     Sab   = random.randint(1, 18)
     Const = random.randint(1, 18)
     
-    Atributos = {"Nome":  Nome, "Atk":  Atk, "Des": Des, "Car": Car, "Int": Int, "Sab": Sab, "Const": Const}
-    print(Atributos)
-    return Raca("Draconato", Atributos)
+    Atributos = {"Nome":  Nome}
+    Atributos["Classe"] = Classe
+    Atributos["Raca"] = RacaEscolhida
+    Atributos.update({"Atk": Atk, "Des": Des, "Car": Car, "Int": Int, "Sab": Sab, "Const": Const})
+    return Raca(Atributos)
 
 
-def Raca(EscolhaRaca, Atributos):
-    if(EscolhaRaca == "Draconato"):
+def Raca(Atributos):
+    if(Atributos["Raca"] == "Draconato"):
         Atributos["Atk"] += 2
         Atributos["Car"] += 2
-    if(EscolhaRaca == "Elfo"):
+        return Classes(Atributos)
+    if(Atributos["Raca"] == "Elfo"):
         Atributos["Des"] += 2
         Atributos["Sab"] += 2
-    # if(EscolhaRaca == "Humano"):
-        # EscolhaAtributo = input("Escolha o atributo que deseja Somar 2 Pontos") Select
-        # Atributos["1"] += 2 Rever
-    if(EscolhaRaca == "Anao"):
+        return Classes(Atributos)
+    if(Atributos["Raca"] == "Humano"):
+        EscolhaAtributo = st.selectbox('Escolha Qual Atributo Deseja Aumentar', [None,'Atk','Des', 'Car', 'Int', 'Sab', 'Const'], index=0)
+        if EscolhaAtributo != None:
+            print("Atributos antes da escolha")
+            print(Atributos)
+            Atributos[EscolhaAtributo] += 2 
+            print(EscolhaAtributo)
+            return Classes(Atributos)
+    if(Atributos["Raca"] == "Anao"):
         Atributos["Const"] += 2
         # Atributos["3"] += 2 Rever
-    if(EscolhaRaca == "Orc"):
+        return Classes(Atributos)
+    if(Atributos["Raca"] == "Orc"):
         Atributos["Atk"] += 2
         Atributos["Const"] += 1
+        return Classes(Atributos)
 
+
+def Classes(Atributos):
+    if(Atributos["Classe"] == "Guerreiro"):
+        Atributos["Atk"] += 2
+        Atributos["Car"] += 2
+    if(Atributos["Classe"] == "Elfo"):
+        Atributos["Des"] += 2
+        Atributos["Sab"] += 2
+    # if(Classe == "Humano"):
+        # EscolhaAtributo = input("Escolha o atributo que deseja Somar 2 Pontos") Select
+        # Atributos["1"] += 2 Rever
+    if(Atributos["Classe"] == "Anao"):
+        Atributos["Const"] += 2
+        # Atributos["3"] += 2 Rever
+    if(Atributos["Classe"] == "Orc"):
+        Atributos["Atk"] += 2
+        Atributos["Const"] += 1
+        
+    print("Atributos")
+    print(Atributos)
     return Atributos
-
-# def Classes():
-#     EscolhaClasse = input("assaas")
 
 # # gerar_valores_aleatorios("luiz")
 # Classes()
