69b4ef5daa5a36f6645071d714e5ab175af29a08
Luiz-Souza0 <luizandre123dede@gmail.com>
2025-10-25 01:31:08 -0300

    update

--- ExibirPersonagemCriado.py
+++ ExibirPersonagemCriado.py
@@ -8,7 +8,7 @@ from Connect.Verify import select_register
 def exibir_personagem_criado(regid: int):
         # Buscar o personagem salvo no banco pelo ID
     atributos_salvos = select_register("personagens", filter={"id": regid}, columns="*")
-
+    st.error("Personagem carregado do banco de dados.")
     if not atributos_salvos:
         atributos_salvos = st.session_state.get('Atributos')
         if not atributos_salvos:
