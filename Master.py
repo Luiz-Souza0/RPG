00c8c8f01abebfb869e98412481cc23932b10527
Luiz-Souza0 <114114974+Luiz-Souza0@users.noreply.github.com>
2025-10-27 18:00:34 -0300

    Fix select_register call by removing filter argument

--- Master.py
+++ Master.py
@@ -2,6 +2,6 @@ import streamlit as st
 from Connect.Verify import select_register
  
 def seeAllPlayers():
-  atributos_salvos = select_register("personagens", filter: dict = None, columns="*")
+  atributos_salvos = select_register("personagens", None, columns="*")
   st.write( atributos_salvos)
 
