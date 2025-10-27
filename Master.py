54130bdb3791b543b387b0d98bbd2feded858c7d
Luiz-Souza0 <114114974+Luiz-Souza0@users.noreply.github.com>
2025-10-27 18:00:21 -0300

    Import select_register from Connect.Verify

--- Master.py
+++ Master.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+from Connect.Verify import select_register
+ 
 def seeAllPlayers():
   atributos_salvos = select_register("personagens", filter: dict = None, columns="*")
   st.write( atributos_salvos)
