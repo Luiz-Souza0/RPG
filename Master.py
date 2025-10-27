cbe8427782a112925c4fb3fa52de2ffe5d323d5b
Luiz-Souza0 <114114974+Luiz-Souza0@users.noreply.github.com>
2025-10-27 17:59:55 -0300

    Update seeAllPlayers function to include filter parameter

--- Master.py
+++ Master.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 def seeAllPlayers():
-  atributos_salvos = select_register("personagens", None, columns="*")
+  atributos_salvos = select_register("personagens", filter: dict = None, columns="*")
   st.write( atributos_salvos)
+
